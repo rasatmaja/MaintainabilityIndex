337fad9fd6624d8d275b51842738487afc855511
rasioatmaja29 <rasioatmaja29@gmail.com>
2018-11-04 12:08:39 +0700

    add controller to clear all data model

---
+++ src/main/java/app/controllers/ClearData.java
@@ -0,0 +1,40 @@
+package app.controllers;
+
+import app.models.*;
+
+import java.lang.reflect.Method;
+
+public class ClearData {
+    ClassProperty classProperty;
+    PredicateNode predicateNode;
+    MethodProperty  methodProperty;
+    ClassEdgeProperty classEdgeProperty;
+    OperandAndOperator operandAndOperator;
+    HalsteadMetricsResult halsteadMetricsResult;
+    CyclomaticComplexityResult cyclomaticComplexityResult;
+    MaintainabilityIndexResult maintainabilityIndexResult;
+
+
+    public ClearData(){
+        classProperty= ClassProperty.getInstance();
+        predicateNode = PredicateNode.getInstance();
+        methodProperty = MethodProperty.getInstance();
+        classEdgeProperty = ClassEdgeProperty.getInstance();
+        operandAndOperator = OperandAndOperator.getInstance();
+        halsteadMetricsResult = HalsteadMetricsResult.getInstance();
+        cyclomaticComplexityResult = CyclomaticComplexityResult.getInstance();
+        maintainabilityIndexResult = MaintainabilityIndexResult.getInstance();
+
+    }
+
+    public void execute(){
+        classProperty.clear();
+        predicateNode.clear();
+        methodProperty.clear();
+        classEdgeProperty.clear();
+        operandAndOperator.clear();
+        halsteadMetricsResult.clear();
+        cyclomaticComplexityResult.clear();
+        maintainabilityIndexResult.clear();
+    }
+}
