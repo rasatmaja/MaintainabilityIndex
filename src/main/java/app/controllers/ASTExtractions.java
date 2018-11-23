cf7fccc0a05c2f19566fc5496b7b8b139a102d64
rasioatmaja29 <rasioatmaja29@gmail.com>
2018-11-23 23:27:09 +0700

    add constructor type

--- src/main/java/app/controllers/ASTExtractions.java
+++ src/main/java/app/controllers/ASTExtractions.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.github.javaparser.JavaParser.parse;
+import static com.github.javaparser.JavaParser.parseJavadoc;
 
 public class ASTExtractions extends Task<Void> {
     private  String FILE_PATH;
@@ -41,7 +42,7 @@ public class ASTExtractions extends Task<Void> {
         filePath.get().entrySet().forEach((entry) -> {
             int key = entry.getKey();
             List<String> values = entry.getValue();
-            updateMessage("Extracting : " + values.get(0));
+            updateMessage("Extracting file: " + values.get(0));
 
             FILE_PATH = values.get(1);
             try {
@@ -51,10 +52,10 @@ public class ASTExtractions extends Task<Void> {
 
             } catch (Exception e) {
                 //e.printStackTrace();
-                System.out.print((char)27 + "[31m" + "[ERROR] : ");
-                System.out.println(FILE_PATH + " : The identifier doesn't represent a java file");
-                System.out.println((char)27 + "[30m");
-                System.out.println();
+                //System.out.print((char)27 + "[31m" + "[ERROR] : ");
+                //System.out.println(FILE_PATH + " : The identifier doesn't represent a java file");
+                //System.out.println((char)27 + "[30m");
+                //System.out.println();
                 ErrorLog.getInstance().set(new ArrayList<>(Arrays.asList(
                         values.get(0),
                         values.get(1),
@@ -69,21 +70,34 @@ public class ASTExtractions extends Task<Void> {
             int dataMethodkey = dataMethodProperty.getKey();
             String className = dataMethodProperty.getValue().get(0);
 
-            System.out.println("Key         : " + dataMethodkey);
+            //System.out.println("Key         : " + dataMethodkey);
             System.out.println("Class name  : " + dataMethodProperty.getValue().get(0));
             System.out.println("Method name : " + dataMethodProperty.getValue().get(1));
-            System.out.println("Body Method : \n" + dataMethodProperty.getValue().get(5));
-            updateMessage("Extracting: " + dataMethodProperty.getValue().get(1));
+            //System.out.println("Body Method : \n" + dataMethodProperty.getValue().get(5));
+            updateMessage("Extracting method: " + dataMethodProperty.getValue().get(6)+"()");
 
             try {
-                String sourceCode = dataMethodProperty.getValue().get(5)
-                        .replaceAll("super\\(.*?\\);", "")
-                        .replaceAll("this\\(.*?\\);", "");
-                BlockStmt bodyMethod = JavaParser.parseBlock(sourceCode);
+                //String sourceCode = dataMethodProperty.getValue().get(5)
+                       // .replaceAll("super\\(.*?\\);", "")
+                        //.replaceAll("this\\(.*?\\);", "");
+               // BlockStmt bodyMethod = JavaParser.parseBlock(sourceCode);
+                if (dataMethodProperty.getValue().get(7).equalsIgnoreCase("constructor")){
+                    String sourceCode = dataMethodProperty.getValue().get(5);
+                    CompilationUnit bodyMethod = JavaParser.parse(sourceCode);
+                    OperandAndOperatorExtraction operandAndOperatorExtraction = new OperandAndOperatorExtraction(className, dataMethodkey);
+                    operandAndOperatorExtraction.visit(bodyMethod, null);
+                    operandAndOperatorExtraction.save();
+                } else if (dataMethodProperty.getValue().get(7).equalsIgnoreCase("method")){
+                    String sourceCode = dataMethodProperty.getValue().get(5);
+                    BlockStmt bodyMethod = JavaParser.parseBlock(sourceCode);
+                    OperandAndOperatorExtraction operandAndOperatorExtraction = new OperandAndOperatorExtraction(className, dataMethodkey);
+                    operandAndOperatorExtraction.visit(bodyMethod, null);
+                    operandAndOperatorExtraction.save();
+                }
 
-                OperandAndOperatorExtraction operandAndOperatorExtraction = new OperandAndOperatorExtraction(className, dataMethodkey);
-                operandAndOperatorExtraction.visit(bodyMethod, null);
-                operandAndOperatorExtraction.save();
+                //OperandAndOperatorExtraction operandAndOperatorExtraction = new OperandAndOperatorExtraction(className, dataMethodkey);
+                //operandAndOperatorExtraction.visit(bodyMethod, null);
+                //operandAndOperatorExtraction.save();
 
                 System.out.println("------------------------------------------------------------------------------");
             } catch (Exception e){
